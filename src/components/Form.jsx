dd04811883b38019b18e79d4a40b14033e552d29
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-02-05 09:55:49 +0300

    init form stayle and sitting regester

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,9 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 const schema = z.object({
-  name: z.string().min(2,{ message: " مطلوب، أدخل اسم من ثلاث محارف فما فوق." })
-  email: z.string().email({message: "أدخل ايميل صحيح"}),
-  pass: z.string().min(8, { message: "يجب أن تكون كلمة المرور 8 محارف" }),
+  name: z.string().min(3, { message: "أدخل اسم أكبر من ثلاثة حروف" }),
+  email: z.string().email({ message: "أدخل ايميل صحيح" }),
+  pass: z.string().min(8, { message: "أدخل كلمة مرور قوية" }),
 });
 
 const Form = () => {
@@ -14,12 +14,86 @@ const Form = () => {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm({resolver : zodResolver(schema)});
+  } = useForm({ resolver: zodResolver(schema) });
 
-  return <>
-  
-  
-  </>;
+  const onSend = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="bg-blue-100 p-3 w-72 rounded-2xl place-self-center mt-20">
+        <form className="w-64" onSubmit={handleSubmit(onSend)}>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              id="name"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              {...register("name")}
+            />
+            <label
+              htmlFor="name"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Name
+            </label>
+            {errors.name && (
+              <p className="text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              {...register("email")}
+              type="text"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email address
+            </label>
+            {errors.email && (
+              <p className="text-red-600">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              {...register("pass")}
+              type="password"
+              id="floating_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+            />
+            <label
+              htmlFor="floating_password"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Password
+            </label>
+            {errors.pass && (
+              <p className="text-red-600">{errors.pass.message}</p>
+            )}
+          </div>
+          <button
+            disabled={!isValid}
+            type="submit"
+            className={
+              !isValid
+                ? "text-white bg-blue-100 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-300 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                : "text-white bg-blue-100  hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-800 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            }
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Form;
