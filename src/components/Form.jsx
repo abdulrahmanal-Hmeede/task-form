8e7ef1d1173e895094c2a7de2006d5fe8f93e65c
Abdulrahman Al-Hmeeede <abdulrahmanhmeedi@gmail.com>
2025-02-05 09:51:43 +0300

    init useForm and zod

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,25 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  name: z.string().min(2,{ message: " مطلوب، أدخل اسم من ثلاث محارف فما فوق." })
+  email: z.string().email({message: "أدخل ايميل صحيح"}),
+  pass: z.string().min(8, { message: "يجب أن تكون كلمة المرور 8 محارف" }),
+});
 
 const Form = () => {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({resolver : zodResolver(schema)});
+
+  return <>
+  
+  
+  </>;
 };
 
 export default Form;
